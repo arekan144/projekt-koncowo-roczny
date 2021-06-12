41ee1040792dad89f293e3a46446addc568d7228
arek <areksa123@o2.pl>
2021-06-12 13:38:14 +0200

    first commit

---
+++ webpack/webpack.config.js
@@ -0,0 +1,48 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js', // bundle.js to wynik kompilacji projektu przez webpacka
+    },
+    devServer: {
+        port: 8080
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            filename: './index.html',
+            title: "page title",
+            template: './src/index.html',
+            tytul: "THREEJS WEBPACK",
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    }
+                }]
+            },
+            {
+                test: /\.(md2)$/i,
+                type: 'asset/resource',
+            }
+        ]
+    },
+    output: {
+        filename: 'bundle.js',
+        sourceMapFilename: "bundle.js.map"
+    },
+    devtool: "source-map",
+    mode: 'development',
+    mode: 'none' // none, development, production
+};
